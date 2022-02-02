d8932bcb8ce51e54bb59af4dc5135fadf34837c9
Sandro-Alessio Gierens <sandro-alessio@gierens.de>
2022-02-02 15:26:38 +0100

    Added threads argument to waitress serve call

--- yubipi.py
+++ yubipi.py
@@ -345,7 +345,7 @@ def main():
                              resource_class_kwargs={'yubikey': yubikey})
             app.config['AUTH_TOKENS'] = args.tokens if args.tokens else []
             # app.run(debug=False, host=args.host, port=args.port)
-            serve(app, host=args.host, port=args.port)
+            serve(app, host=args.host, port=args.port, threads=1)
         finally:
             finalize_gpio()
     else:
