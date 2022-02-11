33c0017e5afbf24e00727e730764e254b2de53a6
Sandro-Alessio Gierens <sandro@gierens.de>
2022-02-11 14:45:59 +0100

    Commented authenticated decorator function.

--- yubipi.py
+++ yubipi.py
@@ -533,21 +533,30 @@ def authenticated(function):
     @wraps(function)
     def decorated(*args, **kwargs):
         token = None
+
+        # get the token from the request headers
         if 'X-Auth-Token' in request.headers:
             token = request.headers['x-auth-token']
+
+        # if no token given return unauthorized
         if not token:
             return make_response(
                 jsonify({'message': 'No authentication token provided.'}),
                 HTTPStatus.UNAUTHORIZED
             )
+
+        # if token not in the authentication tokens return unauthorized
         if ('AUTH_TOKENS' in app.config
                 and token not in app.config['AUTH_TOKENS']):
             return make_response(
                 jsonify({'message': 'Authentication token invalid.'}),
                 HTTPStatus.UNAUTHORIZED
             )
+
+        # if everything fine return the actual endpoint function
         return function(*args, **kwargs)
 
+    # return the decorated function
     return decorated
 
 
